056c4cd795560960772d49ac8bada5a356ed7c60
Peter <schmidt.multimedia@gmail.com>
2021-11-24 09:20:49 +0100

    01. Funktionalität mit Einzelparametern

--- script_01.js
+++ script_01.js
@@ -4,6 +4,21 @@
 
 
 
+/*** 01. Funktionalität mit Einzelparametern */
+// --> "Ich bin Max Mütze."
+output(getSentence("Ich","bin","Maxine","Mützerich"));
+function getSentence(word1,word2,word3,word4) {
+
+    const gap = " ";
+    const punct = ".";
+    const str = word1 + gap +
+                word2 + gap +
+                word3 + gap +
+                word4 +
+                punct;
+
+    return str;
+}
 
 // Modul: Ausgabe in Konsole : Test
 // output("hi"); 
