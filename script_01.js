eb2862c81d85a0430d65762275af0772c9c0dfa1
Peter <schmidt.multimedia@gmail.com>
2021-11-24 11:38:55 +0100

    Theorie: Schleifen (for-schleife)

--- script_01.js
+++ script_01.js
@@ -15,6 +15,25 @@
 // output(arr[2]); 
 // output(arr[arr.length-1]); // Index (Letzte Pos. im Array)
 
+/* 02a. Theorie: Schleifen (for-schleife) */
+
+/* For -Schleife als Wiederholungs-Struktur */
+
+// Inkrement (untere Grenze --> obere Grenze)
+// for (let i = 0; i < 10; i++) {
+//      output(i);
+// }
+
+// Dekrement (obere Grenze --> untere Grenze)
+// for (let i = 10; i > 0; i--) {
+//     output(i);
+// }
+
+// Inkrement (var. Schrittweite)
+for (let i = 0; i < 100; i+=10) {
+    output(i);
+}
+
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
 // output(getSentence("Ich","bin","Maxine","Mützerich"));
@@ -36,9 +55,8 @@ function getSentence(word1,word2,word3,word4) {
 // word1,word2, .... wordN :: Semantische Struktur
 // arr[0],arr[1] ... arr[n] ::Numerische Struktur (0,1,2,3 ... n)
 // --> Transformation  semantisches Problem --> numerisches Problem
-output(getSentenceArr(["Ich","bin","Maxine","Mützerich"]));
+// output(getSentenceArr(["Ich","bin","Maxine","Mützerich"]));
 function getSentenceArr(arr) {
-
     const gap = " ";
     const punct = ".";
     const str = arr[0] + gap +
