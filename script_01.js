1308c82f54ea20f21c591e4282e67e28059167ee
Peter <schmidt.multimedia@gmail.com>
2021-11-24 12:28:24 +0100

    06 / Überlegungen a = a + 1

--- script_01.js
+++ script_01.js
@@ -35,10 +35,56 @@
 // }
 
 /* 02b. For-Schleife für Array-Index (Iteration)*/
-let arr = ["Ich","bin","die","coole","Maxine","Mützerich"];
-for (let i = 0; i < arr.length; i++) {
-     output(arr[i]);
+// let arr = ["Ich","bin","die","coole","Maxine","Mützerich"];
+// for (let i = 0; i < arr.length; i++) {
+//      output(arr[i]);
+// }
+
+ /*********   Überlegungen - Transponierung **********/
+
+/* 
+1. Einer Variablen kann Ihr eigener Wert zugewiesen werden.
+Solange die Variable existiert, bleibt dieser erhalten.
+hier: Aufsummierung
+*/
+
+/* Zu "Fuß" - DRY !!! */
+// let a = 0; // Anfangswert
+// output("inhalt von a: " + a);
+// a = a + 1;  // a = 0 + 1
+// output("inhalt von a: " + a);
+// a = a + 1;  // a = 1 + 1
+// output("inhalt von a: " + a);
+// a = a + 1;  // a = 2 + 1
+// output("inhalt von a: " + a);
+
+/* Besser: mit FOR-Schleife */
+
+// let a = 0; // Anfangswert
+// for (let i = 0; i < 10; i++) {
+//     output("in der loop: " + a);
+//      a = a + 1;
+// }
+// output("nach der loop: " + a);
+
+/* 
+1a. Einer Variablen kann Ihr eigener Wert zugewiesen werden
+Solange die Variable existiert, bleibt dieser erhalten
+hier: Verkettung eines Strings // Transponierung
+*/
+
+let str = ""; // Anfangswert
+const addStr = "Test";
+const gap = " ";
+
+for (let i = 0; i < 5; i++) {
+    // output("in der loop: " + str);
+    str = str + addStr + gap;
 }
+output("nach der loop: " + str);
+
+
+
 
 
 /*** 01. Funktionalität mit Einzelparametern */
@@ -71,6 +117,24 @@ function getSentenceArr(arr) {
                 arr[2] + gap +
                 arr[3] +
                 punct;
+
+    return str;
+}
+
+/*** 01b. Funktionalität mit Array 2  */
+// Transponierung:  untereinander ---> nebeneinander
+// Helge Schneider: Anananandereihung ...
+
+// output(getSentenceArr2(["Ich","bin","Maxine","Mützerich"]));
+
+function getSentenceArr2(arr) {
+    const gap = " ";
+    const punct = ".";
+    let str = "";
+
+    for (let i = 0; i < arr.length; i++) {
+        output(arr[i]);
+    }
 
     return str;
 }
